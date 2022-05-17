cddcb0f9603ee35d968d64f1d294cc10cf73e95d
tungdq <dinhquangtung6954@gmail.com>
2022-05-17 23:14:04 +0700

    fix

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import NumberButton from './components/NumberButtons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start workinsg on your app!</Text>
       <StatusBar style="auto" />
+      <NumberButton number="99"/>
     </View>
   );
 }
